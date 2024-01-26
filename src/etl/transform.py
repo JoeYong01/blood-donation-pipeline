5d0941b8c771086ca7e6792da26c3d5ff580d244
JoeYong01 <joeanselmyz@gmail.com>
2024-01-26 11:48:27 +0800

    fixed typo with get yesterdays_data, added get_file_extension function & added logging

--- src/etl/transform.py
+++ src/etl/transform.py
@@ -1,26 +1,69 @@
 """contains any logic to transform/validate/clean data"""
+import logging
+import os
 from datetime import datetime, timedelta
 import pandas as pd
 
-def get_yesterdays_date(
-    df: pd.DataFrame,
-    date_column: str
-) -> pd.DataFrame:
+logger = logging.getLogger(os.path.basename(__file__))
+
+def get_file_extension(
+    filepath: str
+) -> str:
     """
-    extracts yesterday's data from a pandas dataframe
+    extracts the file extenion type
 
     Args:
-        df (pd.DataFrame): the pandas dataframe in question
-        date_column (str): date column 
+        filepath (str): full path to the file
+
+    Returns:
+        str: extension type
+    """
+    logger.info("running get_file_extension")
+    _, file_ext = os.path.splitext(filepath)
+    file_type = file_ext.lower()[1:]
+    logger.debug("file extension: %s", file_type)
+    logger.info("returning file_type")
+    return file_type
+
+def get_yesterdays_data(
+    filepath: str,
+    date_columns: list
+) -> pd.DataFrame:
+    """
+    extracts yesterday's data from a file
+
+    Args:
+        filepath (str): full path to the file in question
+        date_column (list): a list of possible date columns
 
     Returns:
         pd.DataFrame: pandas dataframe with the latest data
     """
+    logger.info("running get_yesterdays_data function")
     datetime_now = datetime.now()
     datetime_ytd = datetime_now - timedelta(days=1)
     date_ytd = datetime_ytd.strftime("%Y-%m-%d")
-    yesterdays_data = df[pd.to_datetime(df[f"{date_column}"]) == date_ytd]
-    return yesterdays_data
+    logger.debug("obtaining pd.read_* type")
+    pandas_reader = {
+        'csv': pd.read_csv,
+        'parquet': pd.read_parquet
+    }
+    ext_type = get_file_extension(filepath)
+    reader = pandas_reader.get(ext_type)
+    if not reader:
+        error_message = "get_yesterdays_data only supports csv/parquet"
+        logger.exception(error_message)
+        raise ValueError(error_message)
+    df = reader(filepath)
+    for date_column in date_columns:
+        if date_column in df.columns:
+            logger.debug("filtering latest data from pandas dataframe.")
+            yesterdays_data = df[pd.to_datetime(df[f"{date_column}"]) == date_ytd]
+            logger.info("returning pandas dataframe")
+            return yesterdays_data
+    error_message = "date columns not found in dataframe"
+    logger.error(error_message)
+    raise ValueError(error_message)
 
 def validate_col(
     ignore_cols: list,
@@ -38,8 +81,12 @@ def validate_col(
     Returns:
         pd.DataFrame: validated pandas dataframe
     """
+    logger.info("running validate_col function")
     for col in df.columns:
         if col not in ignore_cols:
+            logger.debug("validating other columns are int dtypes")
             df[col] = pd.to_numeric(df[col], errors='coerce')
+            logger.debug("validating int records > 0")
             df[col] = df[col].mask(df[col] < 0)
+    logger.info("returning dataframe")
     return df
