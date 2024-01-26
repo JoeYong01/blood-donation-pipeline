b61e1fbde57a5c31a5b2f63aba7888e8b8f940ea
JoeYong01 <joeanselmyz@gmail.com>
2024-01-26 11:49:08 +0800

    updated main.py to adopt new src modules, updated .gitignore to ignore new download directory

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 from datetime import datetime
 import logging
 import os
-from src.etl.extract import download_file
+from src.etl.extract import (
+    validate_extension,
+    download_file
+)
+from src.etl.transform import (
+	get_yesterdays_data,
+	validate_col,
+)
 
 # misc const/var
-DATE_NOW = datetime.now().strftime("%Y/%m/%d/")
+DATE_NOW = datetime.now().strftime("%Y/%m/%d")
 
 # logging const/var
 LOG_NAME = "blood-donation-pipeline.log"
@@ -20,8 +27,13 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
-# download_file const/var
-DOWNLOAD_DIRECTORY = os.path.join("downloads", DATE_NOW)
+# download_file & directory const/var
+RAW_DIR = os.path.join("data", "raw", DATE_NOW)
+os.makedirs(RAW_DIR, exist_ok=True)
+STAGING_DIR = os.path.join("data", "staging", DATE_NOW)
+os.makedirs(STAGING_DIR, exist_ok=True)
+CLEANED_DIR = os.path.join("data", "cleaned", DATE_NOW)
+os.makedirs(CLEANED_DIR, exist_ok=True)
 FILE_URLS = [
     "https://raw.githubusercontent.com/MoH-Malaysia/data-darah-public/main/donations_facility.csv",
     "https://raw.githubusercontent.com/MoH-Malaysia/data-darah-public/main/donations_state.csv",
@@ -30,12 +42,24 @@ FILE_URLS = [
     "https://dub.sh/ds-data-granular"
 ]
 
+# ignore these columns for int validation
+IGNORE_COLS = ['date', 'hospital', 'state', 'donor_id', 'visit_date']
+DATE_COLS = ['date', 'visit_date']
+
 def main():
     """main function"""
     logger.info("looping through FILE_URLS")
     for url in FILE_URLS:
-        filename = url.rsplit('/', maxsplit=1)[-1]
-        download_file(url, DOWNLOAD_DIRECTORY, filename)
+        # ds-data-granular
+        filename = validate_extension(url.rsplit('/', maxsplit=1)[-1])
+        download_file(url, RAW_DIR, filename)
+        staging_filepath = os.path.join(STAGING_DIR, filename)
+        cleaned_filepath = os.path.join(CLEANED_DIR, filename)
+        filepath = os.path.join(RAW_DIR, filename)
+        df = get_yesterdays_data(filepath, DATE_COLS)
+        df.to_csv(staging_filepath, index=False)
+        df_cleaned = validate_col(IGNORE_COLS, df)
+        df_cleaned.to_csv(cleaned_filepath, index=False)
 
 if __name__ == "__main__":
     main()
